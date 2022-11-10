384715b7f5562269491165c221a71285e4f32c86
34905 <114576132+34905@users.noreply.github.com>
2022-11-10 13:31:22 +0100

    Add files via upload

---
+++ bewerk.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+afbeelding = Image.open("mqdefault.jpg")
+
+afbeelding.show()
+
+breedte = afbeelding.width
+hoogte = afbeelding.height
+
+print("De afbeelding is " + str(breedte) + " pixels breed en " + str(hoogte) + " pixels hoog")
+print(afbeelding.format, afbeelding.size, afbeelding.mode)
+
+helft_breedte = afbeelding.width // 2
+helft_hoogte = afbeelding.height // 2
+
+nieuwe_afmeting = (helft_breedte, helft_hoogte)
+
+kleinere_afbeelding = afbeelding.resize(nieuwe_afmeting)
+
+kleinere_afbeelding.save('sunset_klein.jpg')
